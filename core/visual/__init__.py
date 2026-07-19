ab7aa06915190994b94ea457bfee4e1de432ea7b
farid-gnank02 <mohamedgnank@gmail.com>
2026-07-19 10:45:29 +0200

    feat(visual): add screenshot provider abstraction

--- core/visual/__init__.py
+++ core/visual/__init__.py
@@ -1,4 +1,18 @@
 from core.visual.models import VisualDiffResult
+from core.visual.screenshot import (
+    PlaywrightScreenshotProvider,
+    ScreenshotError,
+    ScreenshotProvider,
+    StaticScreenshotProvider,
+)
 from core.visual.storage import FileSystemScreenshotStorage, ScreenshotStorage
 
-__all__ = ["FileSystemScreenshotStorage", "ScreenshotStorage", "VisualDiffResult"]
+__all__ = [
+    "FileSystemScreenshotStorage",
+    "PlaywrightScreenshotProvider",
+    "ScreenshotError",
+    "ScreenshotProvider",
+    "ScreenshotStorage",
+    "StaticScreenshotProvider",
+    "VisualDiffResult",
+]
