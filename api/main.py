da8c937bf69b34aa2bbe6fd24e50c6cbf85d0720
farid-gnank02 <mohamedgnank@gmail.com>
2026-08-01 16:37:41 +0200

    phase5: add /api/approvals router (list/approve/reject)

--- api/main.py
+++ api/main.py
@@ -16,6 +16,7 @@ from db.base import init_db
 from api.auth.router import router as auth_router
 from api.routers.monitor import router as monitor_router
 from api.routers.integrations import router as integrations_router
+from api.routers.approvals import router as approvals_router
 
 
 @asynccontextmanager
@@ -44,6 +45,7 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(monitor_router)
 app.include_router(integrations_router)
+app.include_router(approvals_router)
 
 
 @app.get("/health")
